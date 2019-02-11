32653effa801796627b2a8093d9d5cb898bfd4cf
Takahiro SHIMIZU <anatofuz@gmail.com>
2019-02-11 17:15:46 +0900

    minil new...

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Pixela - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Pixela;
+
+# DESCRIPTION
+
+WebService::Pixela is ...
+
+# LICENSE
+
+Copyright (C) Takahiro SHIMIZU.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Takahiro SHIMIZU <anatofuz@gmail.com>
---
+++ lib/WebService/Pixela.pm
@@ -0,0 +1,39 @@
+package WebService::Pixela;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Pixela - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Pixela;
+
+=head1 DESCRIPTION
+
+WebService::Pixela is ...
+
+=head1 LICENSE
+
+Copyright (C) Takahiro SHIMIZU.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Takahiro SHIMIZU E<lt>anatofuz@gmail.comE<gt>
+
+=cut
+
